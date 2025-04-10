e5ba2e2b4a57e875522ab0e199eb363192113bcd
Luiz Mário <azure@inforio.com.br>
2025-04-09 22:01:40 -0300

    Initial base code

---
+++ src/main/java/br/com/luizmariodev/ems/temperature/monitoring/EmsTemperatureMonitoringApplication.java
@@ -0,0 +1,13 @@
+package br.com.luizmariodev.ems.temperature.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsTemperatureMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsTemperatureMonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/luizmariodev/ems/temperature/monitoring/EmsTemperatureMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luizmariodev.ems.temperature.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsTemperatureMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
